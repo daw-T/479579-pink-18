6d40aa4c54a66ae84faab37ff6d5b92bffd6c2bc
daw-T <44907469+daw-T@users.noreply.github.com>
2020-01-20 21:26:48 +0300

    исправление ошибок после 1-й проверки

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -1,5 +1,5 @@
 if (false) {
-  throw 'not scritp';
+  throw "not scritp";
 }
 
 var navMain = document.querySelector(".main-nav");
@@ -8,35 +8,35 @@ var pageHeader = document.querySelector(".page-header");
 
 console.log(button);
 
-if (button.classList.contains('page-header__toggle--invisible')) {
-  button.classList.remove('page-header__toggle--invisible');
-  button.classList.add('page-header__toggle--opened');
-  pageHeader.classList.add('stealth');
+if (button.classList.contains("page-header__toggle--invisible")) {
+  button.classList.remove("page-header__toggle--invisible");
+  button.classList.add("page-header__toggle--opened");
+  pageHeader.classList.add("stealth");
 }
-if (navMain.classList.contains('main-nav--opened')) {
-  navMain.classList.remove('main-nav--opened');
-  navMain.classList.add('main-nav--closed');
+if (navMain.classList.contains("main-nav--opened")) {
+  navMain.classList.remove("main-nav--opened");
+  navMain.classList.add("main-nav--closed");
 }
 
-button.addEventListener('click', function () {
+button.addEventListener("click", function () {
   console.log("click botton");
   var button = this;
-  if (button.classList.contains('page-header__toggle--closed')) {
-    button.classList.remove('page-header__toggle--closed');
-    button.classList.add('page-header__toggle--opened');
-    pageHeader.classList.add('stealth');
+  if (button.classList.contains("page-header__toggle--closed")) {
+    button.classList.remove("page-header__toggle--closed");
+    button.classList.add("page-header__toggle--opened");
+    pageHeader.classList.add("stealth");
   } else {
-    button.classList.remove('page-header__toggle--opened');
-    button.classList.add('page-header__toggle--closed');
-    pageHeader.classList.remove('stealth');
+    button.classList.remove("page-header__toggle--opened");
+    button.classList.add("page-header__toggle--closed");
+    pageHeader.classList.remove("stealth");
   }
 
-  if (navMain.classList.contains('main-nav--opened')) {
-    navMain.classList.remove('main-nav--opened');
-    navMain.classList.add('main-nav--closed');
+  if (navMain.classList.contains("main-nav--opened")) {
+    navMain.classList.remove("main-nav--opened");
+    navMain.classList.add("main-nav--closed");
   } else {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
+    navMain.classList.remove("main-nav--closed");
+    navMain.classList.add("main-nav--opened");
   }
 });
 
